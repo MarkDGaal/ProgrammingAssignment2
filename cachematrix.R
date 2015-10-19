ebf9db4fc558c35e84d03478bde789c01d99fe64
MarkDGaal <mark@markgaal.com>
2015-10-19 02:47:28 -0400

    inital code for Peer Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Sets or gets the values in of a matrix and is able to store values for 
+## that matrix outside of the current environment in what is called the execution environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function (y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <-function() x
+        setinverse <- function(solve) m <<- solve ##why would you set the inverse, thats the cacheSolve's job
+        getinverse <- function() m
+        list (set = set, get = get,
+              setinverse = setinverse,
+              getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Computes, caches, and returns inverse of matrix. If the inverse of the matrix is already stored 
+## in the execution environment it returns that stored value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if (!is.null(m)) {
+                message("data is cached, retriving...")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data,...)
+        x$setinverse(m)
+        m
 }
